93ca9ba22a8b70edb0e09f3c1539946a452014b6
FFF-Fox <FFF-Fox@protonmail.com>
2018-07-08 21:33:34 +0300

    Encapsulated V(s) updates in update_values(...)

--- ReinforcementLearning/BlackJack/environment/Agents.py
+++ ReinforcementLearning/BlackJack/environment/Agents.py
@@ -23,13 +23,22 @@ class First_Visit_MC(object):
         self.V = np.zeros(self.K)
         self.Returns = [[] for s in range(self.K)]
 
+    def update_values(self, episode_states, episode_rewards):
+        """ Update the V(s) values after an episode finishes """
+        for s in set(episode_states):
+                i = episode_states.index(s)    # index of the first visit of state s
+                m = len(episode_states) - i
+                R = sum(episode_rewards[-m:])  # total return starting from first visit of s
+                self.Returns[s].append(R)
+                self.V[s] = np.mean(self.Returns[s])
+
     def train(self, env, total_episodes):
         """ Estimate the value of the states that the agent experiences. 
             1. Generate episode using the policy
             2. For each state appeared in the episode keep the return following the first
                occurence and average the Returns of each state visited in this episode. """
-        for episode in range(total_episodes):
-            episode_states = []    # the states that will be visited in the current episode
+        for _ in range(total_episodes):
+            episode_states = []     # the states that will be visited in the current episode
             episode_rewards = []    # the rewards of the episode
 
             env.init_episode()
@@ -41,17 +50,11 @@ class First_Visit_MC(object):
                 episode_rewards.append(reward)
                 episode_states.append(state)
 
-            for s in set(episode_states):
-                i = episode_states.index(s)    # index of the first visit of state s
-                m = len(episode_states) - i
-                R = sum(episode_rewards[-m:])  # total return starting from first visit of s
-                self.Returns[s].append(R)
-                self.V[s] = np.mean(self.Returns[s])
-                
+            self.update_values(episode_states, episode_rewards)
 
 
-if __name__=="__main__":
-    from Env import Env
+if __name__ == "__main__":
+    from environment.Env import Env
 
     env = Env()
     agent = First_Visit_MC()
